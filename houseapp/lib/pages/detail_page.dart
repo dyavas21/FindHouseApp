d1fa97cae191ca908330c47c99b0f3b318ceb1ef
GedeD <dyavas21@gmail.com>
2022-02-17 14:09:04 +0700

    detail page 2

--- houseapp/lib/pages/detail_page.dart
+++ houseapp/lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:houseapp/theme.dart';
 
 class DetailPage extends StatelessWidget {
   @override
@@ -38,10 +39,117 @@ class DetailPage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 328),
+              child: Container(
+                padding: EdgeInsets.only(
+                  left: 24,
+                  top: 30,
+                  bottom: 40,
+                ),
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    title(),
+                  ],
+                ),
+              ),
             )
           ],
         ),
       ),
+    );
+  }
+
+  Widget title() {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Kuretakeso Hott',
+              style: blackTestStyle.copyWith(
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            RichText(
+              text: TextSpan(
+                text: '\$52',
+                style: purpleTestStyle.copyWith(
+                  fontSize: 16,
+                ),
+                children: [
+                  TextSpan(
+                    text: '/ month',
+                    style: greyTestStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Spacer(),
+        Container(
+          margin: EdgeInsets.only(right: 24),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/icon_star.png',
+                width: 20,
+              ),
+              SizedBox(
+                width: 2,
+              ),
+              Image.asset(
+                'assets/icon_star.png',
+                width: 20,
+              ),
+              SizedBox(
+                width: 2,
+              ),
+              Image.asset(
+                'assets/icon_star.png',
+                width: 20,
+              ),
+              SizedBox(
+                width: 2,
+              ),
+              Image.asset(
+                'assets/icon_star.png',
+                width: 20,
+              ),
+              SizedBox(
+                width: 2,
+              ),
+              Image.asset(
+                'assets/icon_star.png',
+                width: 20,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget Facilities() {
+    return Column(
+      children: [],
     );
   }
 }
